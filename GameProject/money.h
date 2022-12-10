f5fe0fb4db59049dc82b87461d6f4934054d11e3
謝沅瓏 <hsiehyuanlung@xieyuanlongde-MacBook-Pro.local>
2022-12-10 15:28:55 +0800

    update money.h

---
+++ GameProject/money.h
@@ -0,0 +1,29 @@
+class player_money{
+
+public:
+
+// constructor
+player_money();
+player_money(int n);
+~player_money();
+
+// set and get
+void money_set(int n);
+int money_get();
+
+// +
+void operator+= (int n);    // _money +=
+player_money operator+ (player_money & m);  // _money + _money
+player_money operator+ (int n);  // _money + int
+friend player_money operator+ (int n, player_money & m);  // int + _money (global)
+
+// -
+bool is_positive(); // if money < 0, set money to 0
+void operator-= (int n);    // _money -=
+player_money operator- (player_money & m);  // _money - _money
+player_money operator- (int n);  // _money - int
+friend player_money operator- (int n, player_money & m);  // int - _money (global)
+
+private:
+    int _money = 0;
+};
